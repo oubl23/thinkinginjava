d96a46b9dd77389b0602025515f4baa1b23fb5e4
gm925 <gm925925@live.com>
2016-11-03 19:44:17 +0800

    add Parcell2

---
+++ src/innerclasses/Parcell2.java
@@ -0,0 +1,40 @@
+package innerclasses;
+
+/**
+ * Created by gm925 on 2016/11/3.
+ */
+public class Parcell2 {
+    class Contests{
+        private int i= 11;
+        public int value(){return i;}
+    }
+    class Destination{
+        private String label;
+        Destination(String whereTo){
+            label = whereTo;
+        }
+        String readLabel(){
+            return label;
+        }
+    }
+    public Destination to(String s){
+        return new Destination(s);
+    }
+    public Contests contests(){
+        return new Contests();
+    }
+    public void ship(String dest){
+        Contests c = contests();
+        Destination d = to(dest);
+        System.out.println(d.readLabel());
+    }
+
+    public static void main(String[] args) {
+        Parcell2 p = new Parcell2();
+        p.ship("tasmania");
+
+        Parcell2 q = new Parcell2();
+        Parcell2.Contests c = q.contests();
+        Parcell2.Destination d= q.to("borneo");
+    }
+}
