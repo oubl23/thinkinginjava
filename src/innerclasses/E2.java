e74d8d30bf2d1a0d3311257e0d012f3c9bbf8bce
oudabao <gm925925@live.com>
2016-11-03 21:25:28 +0800

    add E2

---
+++ src/innerclasses/E2.java
@@ -0,0 +1,23 @@
+package innerclasses;
+
+class StringHolder{
+    private String data;
+    StringHolder(String data){ this.data = data;}
+    public String toString() { return data; }
+}
+
+public class E2{
+    public static void main(String[] args){
+        Sequence sequence = new Sequence(10);
+        for(int i =0; i < 10; i++)
+        {
+            sequence.add(new StringHolder(Integer.toString(i)));
+        }
+
+        Selector selector = sequence.selector();
+        while(!selector.end())
+        {
+            System.out.print(select.current() + " " );
+        }
+    }
+}
